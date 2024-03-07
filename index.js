3748725f3f5b371ab4bbabe338a5994de4434a0f
Dawn F. Thomas <dthomas529@gmail.com>
2024-03-07 18:47:57 -0500

    Update index.js
    Added function to listen on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => { console.log('Server is running on port 3000')});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node API');
+});
